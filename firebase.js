5dbd80552e15eff5ad3eef28b6fb6dd68813c08f
Maria Berestovaya <57846761+antaresofficial@users.noreply.github.com>
2023-06-21 10:22:21 +0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -2,13 +2,13 @@ import firebase from "firebase";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAW2DbGe-2UGWBjTodtVvDd8yOnlm1Vkkg",
-  authDomain: "fast-drake-332019.firebaseapp.com",
-  projectId: "fast-drake-332019",
-  storageBucket: "fast-drake-332019.appspot.com",
-  messagingSenderId: "337873085464",
-  appId: "1:337873085464:web:b126087df56289550ccf6d",
-  measurementId: "G-G0R8H3MQKB"
+  apiKey: "AIzaSyAcYaVbmzhnTNTDyNthgRa-TJ5uoNgpnds",
+  authDomain: "vespertilio-chat.firebaseapp.com",
+  projectId: "vespertilio-chat",
+  storageBucket: "vespertilio-chat.appspot.com",
+  messagingSenderId: "513339664871",
+  appId: "1:513339664871:web:6c0916eb92885ce40df542",
+  measurementId: "G-CYRE7W31PG"
 };
 
 const app = !firebase.apps.length
